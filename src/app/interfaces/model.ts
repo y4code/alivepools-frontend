9a1133edfdc94412dcdd291fe18c7170655f3d18
y4code <8910420+y4code@users.noreply.github.com>
2024-02-18 16:16:17 +0000

    feat(interface): add model

---
+++ src/app/interfaces/model.ts
@@ -0,0 +1,37 @@
+export interface Email {
+  email: string;
+}
+
+export interface EmailWithOtp {
+  email: string;
+  otp: string;
+}
+
+export interface Message {
+  message: string;
+}
+
+export interface Token {
+  token: string;
+}
+
+enum EmailStatus {
+  Verified = 'verified',
+  Unverified = 'unverified',
+}
+
+interface User {
+  id: number;
+  email: string;
+  emailStatus: EmailStatus;
+  password: string;
+}
+
+interface Task {
+  id: number;
+  userId: number;
+  domain: string;
+  email: string;
+  sendFrequency: string;
+  createAt: Date;
+}
