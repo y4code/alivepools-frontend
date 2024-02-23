6758f78c1e9502b6a1374acb500589508b62d4b4
y4code <8910420+y4code@users.noreply.github.com>
2024-02-23 05:15:28 +0000

    feat(signin): page view with form

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,83 @@
+'use client';
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { toast } from "@/components/ui/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { BeautifulBackground } from "../landing/page";
+import useSWR from "swr";
+
+const FormSchema = z.object({
+    email: z.string().min(2, {
+        message: "Email must be at least 2 characters.",
+    }),
+    password: z.string().min(6, {
+        message: "Password must be at least 6 characters.",
+    }),
+})
+
+export default function SigninPage() {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
+    function onSubmit(formData: z.infer<typeof FormSchema>) {
+        console.log(formData);
+        // useSWR
+        // const { data, error, isLoading } = useSWR(
+        //     formData,
+
+
+    }
+    return <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-6">
+        <BeautifulBackground />
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>Sign in</CardTitle>
+                <CardDescription>Keep your website in one-click.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="example@example.com" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input type="password" placeholder="********" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">Sign in</Button>
+                    </form>
+                </Form>
+            </CardContent>
+        </Card>
+
+    </main>
+}
