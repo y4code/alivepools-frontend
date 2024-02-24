055db8f2b332b904f33b506d3ca894f3e79861ae
y4code <8910420+y4code@users.noreply.github.com>
2024-02-24 16:42:08 +0000

    feat(SWR): generics support for SWR

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -6,6 +6,7 @@ import { Message, Res, getErrorMessage } from "../interfaces/model";
 import useSWR from "swr";
 import { getDomain } from "@/lib/api";
 import { BeautifulBackground } from "@/components/custom/beautifulBackground";
+import useSWRMutation from "swr/mutation";
 
 export default function LandingPage() {
     return <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-6">
@@ -18,18 +19,20 @@ export default function LandingPage() {
 
 function InputWithButton() {
     const [domain, setDomain] = useState('');
-    const [queryDomain, setQueryDomain] = useState('');
-    const shouldFetch = queryDomain.trim() !== '';
-    const { data, error, isLoading } = useSWR<Res<Message>>(shouldFetch ? queryDomain : null, getDomain);
-    console.log(data)
+    const shouldFetch = domain.trim() !== '';
+    const { data, error, isMutating, trigger } = useSWRMutation<Res<Message>, Error, { url: any, args: any }, { website: string }>({ url: '/domain', args: { website: domain } }, getDomain)
+
+
     return (
         <div className="flex flex-col w-full max-w-sm items-center space-y-2">
             <div className="flex space-x-2">
                 <Input type="email" placeholder="Enter your domain" value={domain} onChange={e => setDomain(e.target.value)} />
-                <Button onClick={() => setQueryDomain(domain)} type="submit">Check</Button>
+                <Button onClick={() => {
+                    trigger({ website: domain })
+                }} type="submit">Check</Button>
             </div>
             <div className="flex w-full max-w-sm min-h-6 items-center space-x-2">
-                {isLoading && <div>loading...</div>}
+                {isMutating && <div>loading...</div>}
                 {error && <div>failed to load</div>}
                 {data && (
                     <div>
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,30 +1,27 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { setupInterceptors } from './interceptor';
+import { Message, Res } from '@/app/interfaces/model';
+
+const GET = 'GET';
+const POST = 'POST';
+const PUT = 'PUT';
+const DELETE = 'DELETE';
 
 const API_HOST = 'https://alivepools.siri.ink/api';
 const axiosInstance = axios.create({ baseURL: API_HOST, timeout: 10000, });
 setupInterceptors(axiosInstance);
 
-const makeApiRequest = async (endpoint: string, method: 'GET' | 'POST' | 'PUT' | 'DELETE', data?: any): Promise<AxiosResponse<any>> => {
+const makeApiRequest = async (endpoint: string, method: 'GET' | 'POST' | 'PUT' | 'DELETE', data?: any): Promise<any> => {
     const requestOptions: AxiosRequestConfig = {
         method,
         headers: { 'Content-Type': 'application/json' },
         data,
     };
-    const response = await axiosInstance(endpoint, requestOptions);
+    const response = await axiosInstance(API_HOST + endpoint, requestOptions);
     return response.data;
 }
 
-const getFetcher = (url: string): Promise<any> => makeApiRequest(url, 'GET');
-const postFetcher = (url: string, data: any): Promise<any> => makeApiRequest(url, 'POST', data);
-const putFetcher = (url: string, data: any): Promise<any> => makeApiRequest(url, 'PUT', data);
-const deleteFetcher = (url: string): Promise<any> => makeApiRequest(url, 'DELETE');
 
-export const getDomain = (website: string) => postFetcher('/domain', { website });
-export const signIn = (email: string, password: string) => postFetcher('/user/signin', { email, password });
-export const signUp = (email: string, password: string) => postFetcher('/user/signup', { email, password });
-export const signUpConfirmation = (email: string, code: string) => postFetcher('/user/signup/confirmation', { email, code });
-export const getTasks = () => getFetcher('/tasks');
-export const getTaskById = (taskId: string) => getFetcher(`/task/${taskId}`);
-export const updateTaskById = (taskId: string, data: any) => putFetcher(`/task/${taskId}`, data);
-export const deleteTaskById = (taskId: string) => deleteFetcher(`/task/${taskId}`);
+export function getDomain({url, args}: {url: string, args: { website: string }}) {
+    return makeApiRequest(url, POST, args);
+}
