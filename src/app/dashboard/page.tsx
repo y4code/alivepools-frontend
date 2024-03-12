6ac73139323aae174697cb03db06270387be9363
y4code <8910420+y4code@users.noreply.github.com>
2024-03-12 00:42:10 +0000

    feat(dashboard table): render by shadcn table

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@
 import { BeautifulBackground } from "@/components/custom/beautiful-background";
 import useSWR from 'swr';
 import { getTasks } from "@/lib/api";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+
 
 export default function DashboardPage() {
     const { data, error, isLoading } = useSWR({ url: '/tasks' }, getTasks);
@@ -14,13 +16,31 @@ export default function DashboardPage() {
                 {error && <div>Failed to load tasks</div>}
                 {isLoading && <div>Loading...</div>}
                 {data && data.data && data.data.length > 0 && (
-                    <ul>
-                        {data.data.map((task) => (
-                            <li key={task.id} className="border-b border-gray-200 p-4">
-                                <h3 className="font-bold">{task.domain} - {task.status} - {task.created_at}</h3>
-                            </li>
-                        ))}
-                    </ul>
+                    <Table>
+                        <TableCaption>Task List</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Domain</TableHead>
+                                <TableHead>Send Frequency(second)</TableHead>
+                                <TableHead>Status</TableHead>
+                                <TableHead>Created At</TableHead>
+                                <TableHead>Last Run Time</TableHead>
+                                <TableHead>Next Run Time</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {data.data.map((task) => (
+                                <TableRow key={task.id}>
+                                    <TableCell>{task.domain}</TableCell>
+                                    <TableCell>{task.send_frequency}</TableCell>
+                                    <TableCell>{task.status}</TableCell>
+                                    <TableCell>{task.created_at}</TableCell>
+                                    <TableCell>{task.last_run_time}</TableCell>
+                                    <TableCell>{task.next_run_time}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
                 )}
                 {data && !data.data && <div>No tasks found.</div>}
             </div>
